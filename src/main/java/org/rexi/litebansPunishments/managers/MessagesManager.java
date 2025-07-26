0d94bf563a22a7137e47b96457f1a334e313c348
Rexi666 <antonferv@gmail.com>
2025-07-26 14:29:50 +0200

    1.0

--- src/main/java/org/rexi/litebansPunishments/managers/MessagesManager.java
+++ src/main/java/org/rexi/litebansPunishments/managers/MessagesManager.java
@@ -15,10 +15,21 @@ public class MessagesManager {
         return ChatColor.translateAlternateColorCodes('&', msg);
     }
 
-    public static String get(String path, String placeholder, String replacement) {
-        return get(path).replace(placeholder, replacement);
+    public static String get(String path, String... replacements) {
+        String prefix = LitebansPunishments.getInstance().getMessages().getString("prefix", "");
+        String msg = LitebansPunishments.getInstance().getMessages().getString(path, "&cMessage not found: " + path);
+        for (int i = 0; i < replacements.length - 1; i += 2) {
+            msg = msg.replace(replacements[i], replacements[i + 1]);
+        }
+        return ChatColor.translateAlternateColorCodes('&', prefix + msg);
     }
-    public static String getRaw(String path, String placeholder, String replacement) {
-        return get(path).replace(placeholder, replacement);
+
+    public static String getRaw(String path, String... replacements) {
+        String msg = LitebansPunishments.getInstance().getMessages().getString(path, "&cMessage not found: " + path);
+        for (int i = 0; i < replacements.length - 1; i += 2) {
+            msg = msg.replace(replacements[i], replacements[i + 1]);
+        }
+        return ChatColor.translateAlternateColorCodes('&', msg);
     }
+
 }
