50de7a4808ffd8b8b9447622253d0f2710f1f43f
Rexi666 <antonferv@gmail.com>
2025-07-30 15:01:49 +0200

    1.2

--- src/main/java/org/rexi/litebansPunishments/items/ItemBuilder.java
+++ src/main/java/org/rexi/litebansPunishments/items/ItemBuilder.java
@@ -1,5 +1,7 @@
 package org.rexi.litebansPunishments.items;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
 import net.kyori.adventure.text.Component;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -8,12 +10,15 @@ import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.persistence.PersistentDataType;
 import org.rexi.litebansPunishments.LitebansPunishments;
 import org.rexi.litebansPunishments.managers.MessagesManager;
 
+import java.lang.reflect.Field;
 import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
 
 public class ItemBuilder {
     public static ItemStack fromConfig(org.bukkit.configuration.file.FileConfiguration config, String path, String name) {
@@ -21,11 +26,23 @@ public class ItemBuilder {
         if (section == null) {
             return new ItemStack(Material.STONE); // Default item if section is not found
         }
+        String itemString = section.getString("item", "STONE");
+        ItemStack item;
+        if (itemString.startsWith("basehead-")) {
+            String base64 = itemString.substring("basehead-".length());
+            item = getCustomHead(base64);
+        } else {
+            Material mat;
+            try {
+                mat = Material.valueOf(itemString.toUpperCase());
+            } catch (IllegalArgumentException e) {
+                mat = Material.STONE;
+            }
+            item = new ItemStack(mat);
+        }
 
-        Material mat = Material.valueOf(section.getString("item", "STONE"));
         String display = section.getString("name", name);
 
-        ItemStack item = new ItemStack(mat);
         ItemMeta meta = item.getItemMeta();
         meta.setDisplayName(display.replace("&", "§"));
 
@@ -51,17 +68,24 @@ public class ItemBuilder {
     public static ItemStack simpleFromConfig(ConfigurationSection section, String name, String punishKey, String punishTime) {
         if (section == null) return new ItemStack(Material.STONE);
 
-        Material mat;
-        try {
-            mat = Material.valueOf(section.getString("item", "STONE").toUpperCase());
-        } catch (IllegalArgumentException e) {
-            mat = Material.STONE;
+        String itemString = section.getString("item", "STONE");
+        ItemStack item;
+        if (itemString.startsWith("basehead-")) {
+            String base64 = itemString.substring("basehead-".length());
+            item = getCustomHead(base64);
+        } else {
+            Material mat;
+            try {
+                mat = Material.valueOf(itemString.toUpperCase());
+            } catch (IllegalArgumentException e) {
+                mat = Material.STONE;
+            }
+            item = new ItemStack(mat);
         }
 
         String display = section.getString("display", name);
         display = ChatColor.translateAlternateColorCodes('&', display);
 
-        ItemStack item = new ItemStack(mat);
         ItemMeta meta = item.getItemMeta();
         meta.setDisplayName(display);
 
@@ -122,16 +146,23 @@ public class ItemBuilder {
     public static ItemStack fromSimpleSection(ConfigurationSection section, String keyName) {
         if (section == null) return new ItemStack(Material.STONE);
 
-        Material mat;
-        try {
-            mat = Material.valueOf(section.getString("item", "STONE").toUpperCase());
-        } catch (IllegalArgumentException e) {
-            mat = Material.STONE;
+        String itemString = section.getString("item", "STONE");
+        ItemStack item;
+        if (itemString.startsWith("basehead-")) {
+            String base64 = itemString.substring("basehead-".length());
+            item = getCustomHead(base64);
+        } else {
+            Material mat;
+            try {
+                mat = Material.valueOf(itemString.toUpperCase());
+            } catch (IllegalArgumentException e) {
+                mat = Material.STONE;
+            }
+            item = new ItemStack(mat);
         }
 
         String name = ChatColor.translateAlternateColorCodes('&', section.getString("name", keyName));
 
-        ItemStack item = new ItemStack(mat);
         ItemMeta meta = item.getItemMeta();
         meta.setDisplayName(name);
 
@@ -158,6 +189,25 @@ public class ItemBuilder {
 
         item.setItemMeta(meta);
         return item;
+    }
+
+    public static ItemStack getCustomHead(String base64) {
+        ItemStack head = new ItemStack(Material.PLAYER_HEAD);
+        SkullMeta skullMeta = (SkullMeta) head.getItemMeta();
+
+        GameProfile profile = new GameProfile(UUID.randomUUID(), "CustomHead");
+        profile.getProperties().put("textures", new Property("textures", base64));
+
+        try {
+            Field profileField = skullMeta.getClass().getDeclaredField("profile");
+            profileField.setAccessible(true);
+            profileField.set(skullMeta, profile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        head.setItemMeta(skullMeta);
+        return head;
     }
 
 }
